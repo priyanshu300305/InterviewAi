96b6b19fe4c0cac8bdf6479522ccbfbd106b1db6
Jerry <priyanshukumar300305@gmail.com>
2026-09-10 01:21:16 +0530

    changes in auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,14 +1,10 @@
 import axios from "axios"
 
-// const api = axios.create({
-//     baseURL: "http://localhost:3000",
-//     withCredentials: true
-// })
-
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "http://localhost:3000",
     withCredentials: true
 })
+
 
 export async function register({ username, email, password }) {
     const response = await api.post('/api/auth/register', {
